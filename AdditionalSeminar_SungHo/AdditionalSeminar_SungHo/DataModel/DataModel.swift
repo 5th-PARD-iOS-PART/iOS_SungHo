f85fc0642434f8599aafa1513e674d90e8c4b5d6
Lucid <bluecho97@gmail.com>
2025-06-27 00:21:35 +0900

    aa

---
+++ AdditionalSeminar_SungHo/AdditionalSeminar_SungHo/DataModel/DataModel.swift
@@ -0,0 +1,25 @@
+//
+//  DataModel.swift
+//  AdditionalSeminar_SungHo
+//
+//  Created by  Lucid on 6/27/25.
+//
+
+import SwiftUI
+
+
+struct UserInfoModel: Hashable, Identifiable, Decodable {
+    let id: Int
+    let username: String
+    let tastePreferences : [String]
+    let recentStatus: String
+    let yesterdayEaten: String
+    let todayTaste: [String]
+}
+
+extension UserInfoModel {
+    static let mockData: UserInfoModel =
+        UserInfoModel(id: 1, username: "맛있으면 자꾸울어", tastePreferences: ["#맵찔이", "#향신료NO", "#오이 NO"], recentStatus: "다이어트", yesterdayEaten: "오코노미야끼", todayTaste: ["🥩육류", "🍞빵", "🇰🇷한식", "🍽️NO국물", "🥘헤비"])
+    
+}
+
